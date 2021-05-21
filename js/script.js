420545c44ec73a78e2ad43491dadff40638228dc
Ana Sommer <anastassia.sommer@gmail.com>
2021-05-21 17:05:05 +0200

    Added project starter files

---
+++ js/script.js
@@ -0,0 +1,58 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance:
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/***
+ * `quotes` array
+***/
+// 1. Create an Array containing Objects - one object for each quotation
+// 2. Create a Function that pulls one of those quotes out at random
+// 3. Create a Function that prints that quote to the screen
+
+const quotes = [
+    {
+    quote: `There is no influence like the influence og habit.`,
+    source: `Gilbert Parker`
+    },
+    {
+    quote: `Nothing can be done except little by little`,
+    source: `Charles Baudelaire`
+    },
+    {
+    quote: `If you can't stand the heat, get out of the kitchen`,
+    source: `Harry S Truman`
+    },
+    {
+    quote: `Trouble shared is trouble halved`,
+    source: `Lee Iacocca`
+    },
+    {
+    quote: `Havi patience. All things are difficult before they bacome easy`,
+    source: `Saadi`
+    },
+];
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
